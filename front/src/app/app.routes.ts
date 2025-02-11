2cd011cf3716ada741ebe7fc9a0ea8b8a8de6153
LilianeLandau <landauliliane@hotmail.fr>
2025-02-11 12:05:32 +0100

    display users

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -5,13 +5,17 @@ import { Routes } from '@angular/router'; // Importation du type Routes qui perm
 import { LoginComponent } from './login/login.component'; // Importation du composant LoginComponent pour la page de connexion
 import { WelcomeComponent } from './welcome/welcome.component'; // Importation du composant WelcomeComponent pour la page d'accueil après connexion
 import { authGuard } from './guards/auth.guard'; // Importation du guard authGuard qui protège les routes nécessitant une authentification
-
+import { UsersComponent } from './users/users.component'; // Importation du composant UsersComponent pour la page d'affichage des utilisateurs
 // Définition des routes de l'application
 export const routes: Routes = [
   // Route par défaut : redirige vers la page de login
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   // Route pour la page de connexion, le composant LoginComponent sera affiché
   { path: 'login', component: LoginComponent },
+
+
+  // Route pour afficher la liste des utilisateurs
+  { path: 'users', component: UsersComponent },
   // Route protégée : nécessite l'authentification, le composant WelcomeComponent sera affiché uniquement si l'utilisateur est authentifié
   { path: 'welcome', component: WelcomeComponent, canActivate: [authGuard] }
 ];
